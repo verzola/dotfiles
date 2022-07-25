128097da8cb406b640be3f7e2138def934585ba5
Verzola de oliveira machado, Gustavo [CONBR NON-J&J] <GVerzola@its.jnj.com>
2022-07-25 11:10:03 -0300

    Add support for multiple monitors to polybar

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-polybar -r --config=~/.config/polybar/config.ini main
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar -r --config=~/.config/polybar/config.ini main &
+  done
+else
+  polybar --reload example &
+fi
