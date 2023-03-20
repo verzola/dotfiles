4cac921086147c1c1dfc075f7476cb716d263585
Gustavo Verzola <verzola@gmail.com>
2023-03-20 20:29:00 -0300

    Tweak nvim tree

--- nvim/.config/nvim/lua/plugins/tree.lua
+++ nvim/.config/nvim/lua/plugins/tree.lua
@@ -1,1 +1,20 @@
-require('nvim-tree').setup { }
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+require('nvim-tree').setup {
+  view = {
+    --width = 40,
+    float = {
+      enable = true,
+      quit_on_focus_loss = true,
+      open_win_config = {
+        relative = "editor",
+        border = "rounded",
+        width = 40,
+        height = 100,
+        row = 1,
+        col = 1,
+      },
+    },
+  },
+}
